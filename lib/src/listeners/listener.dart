52861d9955dae3ab36353514602ae16db949322b
lamnhan066 <hello@lamnhan.dev>
2023-12-22 12:55:46 +0700

    Improves comments

--- lib/src/listeners/listener.dart
+++ lib/src/listeners/listener.dart
@@ -3,7 +3,12 @@ import 'listener_stub.dart' if (dart.library.html) 'listener_web.dart';
 class SecureGateListener {
   final _delegate = SecureGateListenerImplement();
 
+  /// Get the `stream` of this Listener.
   Stream<void> get stream => _delegate.stream;
+
+  /// Initialize.
   void init() => _delegate.init();
+
+  /// Dispose.
   void dispose() => _delegate.dispose();
 }
